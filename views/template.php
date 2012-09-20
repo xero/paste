c6f99f45f12e439b3be83a607dbb1c64d11b8d69
xero harrison <x@xero.nu>
2012-09-20 12:31:31 -0400

    created a generic template in /views/

---
+++ views/template.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>paste</title>
+		<style type="text/css">
+			*, html {
+				padding: 5px;
+			}
+			body {
+				background: #000;
+				color: #fff;
+				font-family: arial, helvetica, clean, sans-serif;
+			}
+			pre {
+				border: 1px solid #333;
+				background: #ccc;
+				color: #000;
+			}
+		</style>
+	</head>
+	<body>
+		<?=$body; ?>
+	</body>
+</html>
